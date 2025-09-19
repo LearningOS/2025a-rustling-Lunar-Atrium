7a432de111e2856b39fa40da7507914ae0061bf0
Shaopeng Luan <luanshaopeng@outlook.com>
2025-09-19 15:00:34 +1000

    update

--- exercises/traits/traits2.rs
+++ exercises/traits/traits2.rs
@@ -11,11 +11,13 @@
 // I AM NOT DONE
 
 trait AppendBar {
-    fn append_bar(self) -> Self;
+    fn append_bar(&mut self) -> Self;
 }
 
 impl AppendBar for Vec<String> {
-    fn append_bar(self) -> Self {
+    fn append_bar(&mut self) -> Self {
+        self.push(String::from("Bar"));
+        self.to_vec()
     }
 }
 
